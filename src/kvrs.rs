d1c9501f1592cd9157e8afbf016d3cd2ae5b46cd
starovoid <prototyperailgun@gmail.com>
2023-11-02 11:42:24 +0300

    feat: Transferring the --file argument to each of the subcommands

--- src/kvrs.rs
+++ src/kvrs.rs
@@ -3,23 +3,29 @@ use clap::{Arg, Command};
 fn cli() -> Command {
     Command::new("kvrs")
         .name("kvrs")
-        .subcommand(Command::new("get").arg(Arg::new("key").index(1).required(true)))
+        .subcommand(
+            Command::new("get")
+                .arg(Arg::new("key").index(1).required(true))
+                .arg(Arg::new("file").long("file").short('f')),
+        )
         .subcommand(
             Command::new("set")
                 .arg(Arg::new("key").index(1).required(true))
-                .arg(Arg::new("value").index(2).required(true)),
+                .arg(Arg::new("value").index(2).required(true))
+                .arg(Arg::new("file").long("file").short('f')),
         )
         .subcommand(
             Command::new("update")
                 .arg(Arg::new("key").index(1).required(true))
-                .arg(Arg::new("value").index(2).required(true)),
+                .arg(Arg::new("value").index(2).required(true))
+                .arg(Arg::new("file").long("file").short('f')),
         )
         .subcommand(
             Command::new("rm")
                 .arg(Arg::new("key").index(1).required(true))
-                .arg(Arg::new("value").index(2).required(true)),
+                .arg(Arg::new("value").index(2).required(true))
+                .arg(Arg::new("file").long("file").short('f')),
         )
-        .arg(Arg::new("file").long("file").short('f'))
 }
 
 fn main() {
@@ -36,35 +42,39 @@ fn main() {
 
     match operation {
         "get" => {
-            let key = args
+            let _key = args
                 .get_one::<String>("key")
                 .expect("Needs to specify a key: 'kvrs get \"key\"'");
+            let _file = args.get_one::<String>("file");
             todo!()
         }
         "set" => {
-            let key = args
+            let _key = args
                 .get_one::<String>("key")
                 .expect("Needs to specify a key: 'kvrs set \"key\" \"value\"'");
-            let value = args
+            let _value = args
                 .get_one::<String>("value")
                 .expect("Needs to specify a value: 'kvrs set \"key\" \"value\"'");
+            let _file = args.get_one::<String>("file");
             todo!()
         }
         "update" => {
-            let key = args
+            let _key = args
                 .get_one::<String>("key")
                 .expect("Needs to specify a key: 'kvrs update \"key\" \"value\"'");
             let _new_value = args
                 .get_one::<String>("value")
                 .expect("Needs to specify a value: 'kvrs update \"key\" \"value\"'");
+            let _file = args.get_one::<String>("file");
             todo!()
         }
         "rm" => {
-            let key = args
+            let _key = args
                 .get_one::<String>("key")
                 .expect("Needs to specify a key: 'kvrs rm \"key\"'");
+            let _file = args.get_one::<String>("file");
             todo!()
         }
-        _ => unreachable!()
+        _ => unreachable!(),
     }
 }
