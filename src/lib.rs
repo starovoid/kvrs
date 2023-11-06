cdbc6dca4ab1f8cdedfa807ca9b42c380a544970
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-06 12:24:41 +0300

    move errors to `error` module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+pub mod error;
+
 use byteorder::{BigEndian, ReadBytesExt};
 use indexmap::IndexMap;
-
-use std::fmt;
 use std::fs::{File, OpenOptions};
-use std::io::{self, Cursor, Read, Seek, SeekFrom};
+use std::io::{Cursor, Read, Seek, SeekFrom};
 use std::path::Path;
+use crate::error::{DataFormatError, StorageError};
 
 type Index = IndexMap<Vec<u8>, u64>;
 
@@ -15,48 +16,6 @@ const IDENTIFIER: u64 = 14175028930806269345;
 
 /// The oldest version of the data file format.
 const OLDEST_VERSION: u8 = 1;
-
-/// Top-level type of library error.
-#[derive(Debug, PartialEq)]
-pub enum StorageError {
-    /// I/O error with kinds from `std::io`.`
-    IO(io::ErrorKind),
-
-    /// Wrong data format.
-    DataFormat(DataFormatError),
-
-    /// Failed to load index.
-    FailedLoadIndex,
-}
-
-impl fmt::Display for StorageError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::IO(e) => write!(f, "{e}"),
-            Self::DataFormat(e) => write!(f, "Data format error: {e}"),
-            Self::FailedLoadIndex => write!(f, "Failed to load index"),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq)]
-// Possible data format errors.
-pub enum DataFormatError {
-    /// Missing identifier of the bytes `c4 b7 d1 b5 c5 97 c5 a1` (the first 8 bytes of data).
-    MissedIdentifier,
-
-    /// Incorrect version number is specified (byte with index 8 from the beginning of the data).
-    IncorrectVersion(u8),
-}
-
-impl fmt::Display for DataFormatError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::MissedIdentifier => write!(f, "Missing identifier at the beginning of the data file."),
-            Self::IncorrectVersion(n) => write!(f, "Incorrect version number of the data file format: {n}. The older version has the number {OLDEST_VERSION}"),
-        }
-    }
-}
 
 #[derive(Debug)]
 pub struct Storage<T> {
